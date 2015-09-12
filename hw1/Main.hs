d6984742b580726d1f842c6a22ef498547606d50
Dominik Bollmann <dominikbollmann@gmail.com>
2015-09-12 13:12:45 -0400

    [hw #1] remove redundant type annotations.

--- hw1/Main.hs
+++ hw1/Main.hs
@@ -133,9 +133,9 @@ takeWhile p (x:xs)
 
 ttakeWhile :: Test
 ttakeWhile = "takeWhile" ~: TestList
-  [ takeWhile (< 3) [1,2,3,4,1,2,3,4] ~?= ([1,2] :: [Int])
-  , takeWhile (< 9) [1,2,3] ~?= ([1,2,3] :: [Int])
-  , takeWhile (< 0) [1,2,3] ~?= ([] :: [Int])
+  [ takeWhile (< 3) [1,2,3,4,1,2,3,4] ~?= [1,2]
+  , takeWhile (< 9) [1,2,3] ~?= [1,2,3]
+  , takeWhile (< 0) [1,2,3] ~?= []
   ]
 
 
